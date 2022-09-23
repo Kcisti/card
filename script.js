c0cb7ef86955231c4bdcb98830da5890a370877c
Kcisti <91692567+Kcisti@users.noreply.github.com>
2022-09-23 20:58:20 +0200

    456568987909

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const DOM = {
+  profile : document.querySelector('.profile'),
+  profileHead : document.querySelector('.profile_head'),
+  profileFooterCodes : document.querySelector('.profile_codes'),
+  profileBody : document.querySelector('.profile_body'),
+}
 
+function fullprofile(){
+  DOM.profileBody.classList.toggle('profile_body_close');
+  DOM.profileFooterCodes.classList.toggle('profile_codes_close');
+  DOM.profile.classList.toggle('profile_close');
+}
+
+DOM.profile.addEventListener('click',fullprofile);
+window.addEventListener('load',fullprofile)
